20750afc2b376912b3df79b7721ff2958ec14a64
AayushKhanal47 <aayushkhanal810@gmail.com>
2025-08-13 10:50:56 +0530

    fix:added cookies

--- backend/src/middlewares/jwtVerifyMiddleware.ts
+++ backend/src/middlewares/jwtVerifyMiddleware.ts
@@ -5,20 +5,24 @@ export const jwtVerifyMiddleware = async (
   c: Context,
   next: () => Promise<void>
 ) => {
+  let token: string | undefined;
   const authHeader = c.req.header("Authorization");
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    token = authHeader.substring(7);
+  }
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  if (!token) {
+    token = c.req.cookie("token");
+  }
+
+  if (!token) {
     c.status(401);
     return c.json({ error: "Authorization token missing" });
   }
 
-  const token = authHeader.substring(7);
-
   try {
     const payload = await verify(token, c.env.JWT_SECRET);
-
     c.set("user", payload);
-
     await next();
   } catch (err) {
     c.status(401);
